442e2485f354ac09dc0f7a6970e6c138a5cc735b
sarvasvkulpati <sarvasvkulpati@gmail.com>
2018-05-19 22:20:21 +0800

    added timing for smile

--- smile.py
+++ smile.py
@@ -12,18 +12,13 @@ import numpy as np
 
 def print_indicator(data, model, class_names, bar_width=50):
     probabilities = model.predict(np.array([data]))[0]
-    print(probabilities)
+    #print(probabilities)
     left_count = int(probabilities[1] * bar_width)
     right_count = bar_width - left_count
     left_side = '-' * left_count
     right_side = '-' * right_count
-    interval = time.time() + 10
-    while probabilities[1] > 0.5 :
-        print("smiling")
-        if time.time() > interval:
-            print("you smiled for 10 seconds")
-            break
-    print class_names[0], left_side + '###' + right_side, class_names[1]
+    #print class_names[0], left_side + '###' + right_side, class_names[1]
+    return probabilities[1]
 
 
 class_names = ['Neutral', 'Smiling']
@@ -38,17 +33,22 @@ import cv2
 
 classifier = cv2.CascadeClassifier('lbpcascade_frontalface.xml')
 
+smileStart = 0
+checkingSmile = False
+interval = 5
+
+
 video_capture = cv2.VideoCapture(0)
 try:
     while True:
+        currentTime = time.time()
         ret, frame = video_capture.read()
         cv2.imshow('webcam', frame)
         cv2.waitKey(1)
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        t0 = time.time() 
+       
         faces = classifier.detectMultiScale(gray, 1.3, 5)
-        t1 = time.time()
-        print(t1 - t0)
+       
         if len(faces) != 0:
             (x,y,w,h) = faces[0]
                 
@@ -65,7 +65,24 @@ try:
             data = detected_face[:, :, np.newaxis]
             data = data.astype(np.float) / 255
 
-            print_indicator(data, model, class_names)
+            prob = print_indicator(data, model, class_names)
+            if prob > 0.5 and not checkingSmile:
+                print("started smile")
+                smileStart = time.time()
+                checkingSmile = True
+            elif prob > 0.5 and checkingSmile:
+                elapsed = currentTime - smileStart 
+                print(str(currentTime) + " - " + str(smileStart + interval))
+                print("smiled for " + str(elapsed))
+                if elapsed > interval:
+                    print("you smiled for 5 seconds")
+                    checkingSmile = False
+                    smileStart = 0
+            else:
+                checkingSmile = False
+                print("not smiling")
+    
+            
         else:
             print("no face detected")
 
